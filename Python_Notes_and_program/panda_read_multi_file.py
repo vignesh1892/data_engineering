6272d837f6446f6b78bcd4930f2ecfcce27f6ca1
vignesh1892 <vigneshkumarifet.com>
2025-09-21 19:35:20 +0530

    FIle handlings

--- Python_Notes_and_program/panda_read_multi_file.py
+++ Python_Notes_and_program/panda_read_multi_file.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import glob
 import zipfile
+import rarfile
 
-files_list_rel = glob.glob("F:/Studies/Big_Data/Inceptez/Azure_AI_Class/python_notes_learnings/trainings/github/data_engineering/Python_Notes_and_program/*.csv")
+files_list_rel = glob.glob(
+    "F:/Studies/Big_Data/Inceptez/Azure_AI_Class/python_notes_learnings/trainings/github/data_engineering/Python_Notes_and_program/sample_data/*.csv"
+)
 print("files_list (relative):", files_list_rel)
+
 
 print("files_list=",files_list_rel)
 all_df=[]
@@ -13,8 +17,19 @@ for csvfile in files_list_rel:
 
 print(all_df)
 
+final_df= pd.concat(all_df,ignore_index=True)
+
+print(final_df)
+
 ##zip file
-with zipfile.ZipFile("Python_Notes_and_program/sample_data.zip","r") as z:
-    for filename in z.namelist:
-        print("filename",filename)
+zip_path = "F:/Studies/Big_Data/Inceptez/Azure_AI_Class/python_notes_learnings/trainings/github/data_engineering/Python_Notes_and_program/sample_data_zip.rar"
+
+all_df = []
+
+with rarfile.RarFile(zip_path, "r") as z:
+    for filename in z.namelist():
+        if filename.endswith(".csv"):
+            with z.open(filename) as f:
+                df = pd.read_csv(f)
+                all_df.append(df)
 
